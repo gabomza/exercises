8ec83c2a0e750eb31a1e0eaea0e084d4db4609a7
gabomza <gaboguzman@gmail.com>
2022-11-30 12:05:48 -0300

    initial commit, url parser exercise.

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { isEqual } from "lodash";
+
+const generateHash = (urlFormat: string, urlInstance: string) => {
+  urlFormat = urlFormat.trim();
+  urlInstance = urlInstance.trim();
+  if (!urlFormat || !urlInstance) {
+    console.error("url format and url instance are invalid.");
+    return null;
+  }
+  // Separate the parts and remove any empty string
+  const splittedUrlFormat = urlFormat.split("/").filter((item) => item !== "");
+  const splittedUrlInstance = urlInstance
+    .split("/")
+    .filter((item) => item !== "");
+
+  if (splittedUrlFormat.length !== splittedUrlInstance.length) {
+    console.warn("strings should have the same format");
+    return null;
+  }
+
+  const hash = {};
+
+  splittedUrlFormat.forEach((part, index) => {
+    if (part.startsWith(":")) {
+      if (splittedUrlInstance[index].includes("?")) {
+        const [lastPart, queryParamsString] = splittedUrlInstance[index].split(
+          "?"
+        );
+        if (lastPart) {
+          const key = part.substring(1);
+          hash[key] = lastPart;
+        }
+
+        if (queryParamsString) {
+          if (queryParamsString.includes("&")) {
+            const queryParams = queryParamsString.split("&");
+            queryParams.forEach((param) => {
+              const [key, value] = param.split("=");
+              hash[key] = value;
+            });
+          } else {
+            const [key, value] = queryParamsString.split("=");
+            hash[key] = value;
+          }
+        }
+      } else {
+        const key = part.substring(1);
+        hash[key] = splittedUrlInstance[index];
+      }
+    }
+  });
+
+  return hash;
+};
+
+const sampleUrlFormat = "/:version/api/:collection/:id";
+const sampleUrlInstance = "/6/api/listings/3?sort=desc&limit=10";
+
+const sampleHash = {
+  version: "6",
+  collection: "listings",
+  id: "3",
+  sort: "desc",
+  limit: "10"
+};
+
+const hash = generateHash(sampleUrlFormat, sampleUrlInstance);
+
+console.log(isEqual(hash, sampleHash));
